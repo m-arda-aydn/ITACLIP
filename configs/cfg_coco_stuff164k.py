497b72fb6d91de661ac23a8e3177bcef73d758f1
Mustafa Arda Aydın <76566169+m-arda-aydn@users.noreply.github.com>
2026-02-01 00:06:06 +0300

    Update paths to use relative directory references

--- configs/cfg_coco_stuff164k.py
+++ configs/cfg_coco_stuff164k.py
@@ -6,7 +6,7 @@ model = dict(
     type='ITACLIP_Segmentor',
     model_name = 'ViT-B/16',
     img_engineering = True,
-    auxiliary_text_path = f'/ITACLIP/llama_generated_texts/{dataset_name}_definitions.txt',
+    auxiliary_text_path = f'./ITACLIP/llama_generated_texts/{dataset_name}_definitions.txt',
     dataset_name = dataset_name,
     slide_stride = 28,
     attn_self = True,
@@ -15,7 +15,7 @@ model = dict(
     width_chunk_size = 150, # This variable helps reduce GPU memory consumption when the text expansion technique is applied. The default values are optimized for a 24 GB GPU under this configuration.
     pamr_steps = 10,
     device = 'cuda:0',
-    name_path=f'/ITACLIP/configs/cls_{dataset_name}.txt',
+    name_path=f'./ITACLIP/configs/cls_{dataset_name}.txt',
     logit_scale = 40,
 )
 
